f9999b811d04e87497a35bf78f685ce3195529c2
Gibbobn <danielmcgowan@outlook.com>
2026-07-25 01:51:12 +1200

    added tween for camera when player freezes

--- level.gd
+++ level.gd
@@ -12,4 +12,9 @@ func _on_timer_timeout() -> void:
 	var child = player.instantiate()
 	child.position = Vector2(100,450)
 	add_child(child)
-	
+	var cur = get_tree().get_nodes_in_group("Playing")[0]
+	cur.get_node("Camera").reparent(child)
+	var tween = create_tween()
+	tween.tween_property(child.get_node("Camera"), "position", Vector2.ZERO, 1)
+	cur.remove_from_group("Playing")
+	child.add_to_group("Playing")
